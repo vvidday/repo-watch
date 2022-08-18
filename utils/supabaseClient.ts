9c66ccdd81628d5995772ab91b875792f88aa565
vvidday <davids89034@gmail.com>
2022-08-18 21:45:44 +0800

    chore(deps): downgrade supabase-js to stable version (bugs w/realtime)

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 import { Database } from './types'
 
-export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
-)
+export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL ?? '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '')
