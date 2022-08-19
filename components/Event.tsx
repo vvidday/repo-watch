014a45bc6e61d100a4d9dce5b931608fe63d82bb
vvidday <davids89034@gmail.com>
2022-08-19 18:23:38 +0800

    feat: display time since event

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { getIconFromType, getSumamryHeaderFromType, getSummaryFromEvent } from '../utils/helpers'
 import { EventInfo } from '../utils/types'
 import remarkGfm from 'remark-gfm'
 import { MarkGithubIcon } from '@primer/octicons-react'
+import Moment from 'react-moment'
 
 const Event: FC<{
   ev: EventInfo
@@ -14,15 +14,22 @@ const Event: FC<{
   const summaryHeader = getSumamryHeaderFromType(ev.type)
 
   return (
-    <details className="md:w-4/5 m-auto">
+    <details className="lg:w-4/5 m-auto">
       <summary className="flex items-center mx-5 my-3 cursor-pointer justify-between">
         <div className="flex items-center">
           {icon}
           {summary}
         </div>
-        <a href={ev.url} className="ml-3" target="_blank">
-          <MarkGithubIcon size={24} />
-        </a>
+        <div className="flex">
+          <div className="hidden lg:block cursor-text">
+            <Moment fromNow interval={60000}>
+              {ev.created_at}
+            </Moment>
+          </div>
+          <a href={ev.url} className="ml-3" target="_blank">
+            <MarkGithubIcon size={24} />
+          </a>
+        </div>
       </summary>
       <div className="bg-slate-800">
         <p className="ml-5 font-bold">{ev.body === null || ev.body === '' ? '-' : summaryHeader}</p>
