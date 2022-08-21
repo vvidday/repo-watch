73e074cc7f269e9e7b30381d5975d98f0a88816d
vvidday <davids89034@gmail.com>
2022-08-21 15:08:46 +0800

    feat: filter events based on provided filter

--- components/Events.tsx
+++ components/Events.tsx
@@ -1,9 +1,34 @@
-import { FC } from 'react'
-import { EventInfo } from '../utils/types'
+import { FC, useMemo } from 'react'
+import { EventInfo, Filter } from '../utils/types'
 import Event from './Event'
 
-const Events: FC<{ events: EventInfo[]; getRepoNameFromId: (id: number) => string }> = ({ events, getRepoNameFromId }) => {
-  const latestEvents = events.slice(0, 50)
+const filterEvent = (event: EventInfo, filter: Filter): boolean => {
+  if (filter.include_only_name.size > 0 && !filter.include_only_name.has(event.actor_username)) {
+    return false
+  }
+  const excludedUsername = filter.exclude_name.has(event.actor_username)
+  const excludedEventType = filter.exclude_event_type.has(event.type)
+  const excludedRepo = filter.exclude_repo.has(event.repo_id)
+  return !excludedUsername && !excludedEventType && !excludedRepo
+}
+
+const getFilteredEvents = (events: EventInfo[], filter: Filter, limit: number) => {
+  const result: EventInfo[] = []
+  for (const event of events) {
+    if (filterEvent(event, filter)) {
+      result.push(event)
+      if (result.length >= limit) break
+    }
+  }
+  return result
+}
+
+const Events: FC<{ events: EventInfo[]; getRepoNameFromId: (id: number) => string; filter: Filter }> = ({
+  events,
+  getRepoNameFromId,
+  filter,
+}) => {
+  const latestEvents = useMemo(() => getFilteredEvents(events, filter, 100), [events, filter])
   return (
     <div>
       {latestEvents.map((ev, i) => {
