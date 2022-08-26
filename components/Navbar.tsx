6db0a2773a2a75d93cebb34b51aa5bf505190381
David <davids89034@gmail.com>
2022-08-26 08:03:46 +0800

    style: add proper styles for light mode

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Dispatch, FC, SetStateAction } from 'react'
 
 const Navbar: FC<{ page: number; setPage: Dispatch<SetStateAction<number>> }> = ({ page, setPage }) => {
   return (
-    <nav className="bg-black fixed top-0 w-full h-14 z-10 flex items-center justify-center">
+    <nav className="bg-white dark:bg-black fixed top-0 w-full h-14 z-10 flex items-center justify-center">
       <div className="absolute left-0 2xl:left-[8.33333%] flex items-center">
         <Image src="/logo.png" alt="logo" width="50px" height="50px" />
         <p className="hidden md:block ml-2 pt-1 text-2xl font-bold">Repo Watch</p>
